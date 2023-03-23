a2bd3e81585b8b9bacedea9397c7de86ebcf6c04
shibayashihiro <devstack777@gmail.com>
2023-03-23 14:53:24 -0500

    fdsfds

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,7 +1,5 @@
-// plugins/amplify.js
 import Vue from 'vue'
-import '@aws-amplify/ui-vue'
-import Amplify, { Auth } from 'aws-amplify'
+import { Amplify } from 'aws-amplify';
 Amplify.configure({
     aws_project_region: 'us-east-1' // (optional) Default region for project
   });
