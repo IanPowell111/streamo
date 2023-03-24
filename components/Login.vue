d6634ce54c6d6efb62fd8f33341d50c1a56f8665
Vasylenko0908 <devstack777@gmail.com>
2023-03-24 21:56:41 +0100

    dfdf

--- components/Login.vue
+++ components/Login.vue
@@ -77,8 +77,7 @@ export default {
         }
         console.log(user)
       } catch (err) {
-        console.log(err)
-        this.$toast.global.my_error() //Using custom toast
+        console.log(err){
         this.$toast.error('Error while authenticating')
       }
     },
