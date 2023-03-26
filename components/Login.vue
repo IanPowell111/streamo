4384551d58025ca08d6df5f2d6774085c8bd502c
Vasylenko0908 <devstack777@gmail.com>
2023-03-26 18:10:53 +0200

    sdsd

--- components/Login.vue
+++ components/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="lg:w-7/12 md:w-5/6 w-full mx-auto">
         <div class="border-1 border-[#333333] p-5 sm:p-[60px]">
-            <div id="googleButton" @click="googlelogin">Sign In with Google</div>
+            <Button id="googleButton" @click="googlelogin">Sign In with Google</Button>
             <div id="facebookButton"></div>
             <div id="twitterButton"></div>
             <form @submit.prevent="userLogin">
